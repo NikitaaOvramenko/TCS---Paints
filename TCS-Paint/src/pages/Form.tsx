52dd4c82520c13b191f3bf7b3919aa7d99cbf9f3
NikitaOvramenko <ovramenko.nikitka@gmail.com>
2025-08-14 16:43:41 -0700

    check

--- TCS-Paint/src/pages/Form.tsx
+++ TCS-Paint/src/pages/Form.tsx
@@ -5,7 +5,7 @@ import Footer from "../footer";
 export default function Form() {
   return (
     <>
-      <div className="form  w-full min-h-screen flex gap-8 flex-col items-center  bg-radial-[at_50%_100%] from-[rgba(140,28,49,1)] to-[rgba(12,12,26,1)] to-75% ">
+      <div className="form  w-full h-full flex gap-8 flex-col items-center  bg-radial-[at_50%_100%] from-[rgba(140,28,49,1)] to-[rgba(12,12,26,1)] to-75% ">
         <GapCard height="h-[80px]" />
         <FormTemplate className="w-[75%] h-full bg-[rgba(3,0,0,0.72)]  " />
         <Footer />
