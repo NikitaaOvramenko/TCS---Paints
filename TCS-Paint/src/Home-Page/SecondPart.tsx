56bb095e21ba1467aafc204ff9f2dd6746895af6
NikitaOvramenko <ovramenko.nikitka@gmail.com>
2025-08-17 15:28:34 -0700

    edited home page

--- TCS-Paint/src/Home-Page/SecondPart.tsx
+++ TCS-Paint/src/Home-Page/SecondPart.tsx
@@ -7,48 +7,51 @@ import GarageAfter from "../assets/after/garage-after.jpg";
 import { useEffect, useState } from "react";
 
 export default function SecondPart() {
-  const [width, SetWidth] = useState(window.innerWidth >= 1024 ? 500 : 180);
+  const [width, SetWidth] = useState(window.innerWidth >= 1024 ? 500 : 350);
 
   useEffect(() => {
     const handleResize = () => {
-      SetWidth(window.innerWidth >= 1024 ? 500 : 180);
+      SetWidth(window.innerWidth >= 1024 ? 500 : 350);
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <div className="intro h-[512px] md:h-[1024px] w-full flex flex-col items-center justify-center gap-8 bg-radial-[at_75%_50%] from-[rgba(140,28,49,1)] to-[rgba(12,12,26,1)] to-50%">
-      <div className="partOne flex flex-row md:flex-row gap-8">
+    <div className="intro min-h-[1024px] md:min-h-[1024px] w-full flex flex-col items-center justify-center gap-8 bg-radial-[at_75%_50%] from-[rgba(140,28,49,1)] to-[rgba(12,12,26,1)] to-50%">
+      {/* Top section */}
+      <div className="partOne flex flex-col lg:flex-row gap-8 w-full max-w-5xl grow  lg:grow-0 items-center">
         <TextPanel
           text={["Prep,Paint,Perfect.", "No Shortcuts Taken.", "No Mess Left."]}
           color="#16020283"
-          width="w-[180px] lg:w-[500px] "
-          height="h-[180px] lg:h-[400px]"
-          font="text-1xl  lg:text-4xl"
+          width="w-[400px] lg:w-[500px] "
+          height="h-[200px] lg:h-[400px]"
+          font="text-2xl sm:text-3xl lg:text-4xl"
         />
         <BeforeAfterCard
           beforeSrc={DoorBefore}
           afterSrc={DoorAfter}
-          width="w-[180px] lg:w-[500px] "
-          height="h-[180px] lg:h-[400px]"
+          width="w-[400px] lg:w-[500px] "
+          height="h-[250px] lg:h-[400px]"
           realWidth={width}
         />
       </div>
-      <div className="partTwo flex flex-row md:flex-row gap-8">
+
+      {/* Bottom section */}
+      <div className="partTwo flex flex-col-reverse lg:flex-row h-full grow  lg:grow-0 gap-8 w-full max-w-5xl items-center">
         <BeforeAfterCard
           beforeSrc={GarageBefore}
           afterSrc={GarageAfter}
-          width="w-[180px] lg:w-[500px] "
-          height="h-[180px] lg:h-[400px]"
+          width="w-[400px] lg:w-[500px] "
+          height="h-[250px] lg:h-[400px]"
           realWidth={width}
         />
         <TextPanel
           text={["We Paint Doors.", "We Paint Walls.", "We Paint It All."]}
           color="#16020283"
-          width="w-[180px] lg:w-[500px] "
-          height="h-[180px] lg:h-[400px]"
-          font="text-1xl  lg:text-4xl"
+          width="w-[400px] lg:w-[500px] "
+          height="h-[200px] lg:h-[400px]"
+          font="text-2xl sm:text-3xl lg:text-4xl"
         />
       </div>
     </div>
--- TCS-Paint/src/pages/Home.tsx
+++ TCS-Paint/src/pages/Home.tsx
@@ -2,12 +2,14 @@ import Intro from "../Home-Page/Intro";
 import SecondPart from "../Home-Page/SecondPart";
 import Guide from "../Home-Page/Guide";
 import Footer from "../footer";
+import GapCard from "../custom-components/GapCard";
 export default function Home() {
   return (
     <>
       <div className="self-center home bg-[rgba(12,12,26,1)] w-full h-min-screen flex flex-col items-center  ">
         <Intro />
         <SecondPart />
+        <GapCard height="h-[20px] lg:h-[0px]" />
         <Guide />
         <Footer />
       </div>
