89970e4c902ce99b9f4cb5769f71dc271137ad54
NikitaOvramenko <ovramenko.nikitka@gmail.com>
2025-11-03 15:51:36 -0500

    added to footer

--- TCS-Paint/src/footer.tsx
+++ TCS-Paint/src/footer.tsx
@@ -1,8 +1,11 @@
+import instaIcon from "./assets/icons/instagram(1).png";
+import thumbIcon from "./assets/icons/thumbtack.png";
+
 export default function Footer() {
   return (
     <footer className="w-full bg-black text-white py-6 ">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between px-4 gap-4">
-        <div className="flex flex-col gap-6 text-lg md:text-base font-semibold">
+        <div className="flex flex-col md:flex-col gap-2 text-lg md:text-base font-semibold">
           <a
             href="https://wa.me/14243464307"
             target="_blank"
@@ -11,17 +14,29 @@ export default function Footer() {
           >
             📞 424-346-4307
           </a>
-          {/* <a href="mailto:johndoe@maksym.ovramenko@gmail.com">
-            maksym.ovramenko@gmail.com
-          </a> */}
+          <a href="mailto:tcspaintsfl@gmail.com">tcspaintsfl@gmail.com</a>
+          <div className="socials flex flex-row gap-2">
+            <a
+              href="https://www.instagram.com/your_local.painter.and.co?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+              className="insta"
+              target="_blank"
+            >
+              <img src={instaIcon} width={35} height={35} alt="" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.thumbtack.com/fl/north-miami-beach/construction-estimating/your-local-painter-co/service/456721325265764352?utm_medium=web&utm_source=txt&surface=sp"
+              className="thumbtack"
+            >
+              <img src={thumbIcon} width={35} height={35} alt="" />
+            </a>
+          </div>
         </div>
         {/* Left section */}
         <div className="flex flex-col items-center md:items-start text-sm md:text-base gap-1">
-          <p>
-            &copy; {new Date().getFullYear()} Technological Construction Service
-          </p>
+          <p>&copy; {new Date().getFullYear()} Your Local Painter & Co</p>
           <p>All rights reserved.</p>
-          <p>Los Angeles, CA</p>
+          <p>Miami, FL</p>
         </div>
 
         {/* Right section */}
